57079540bb0434caa43436fc965f5b4529d646ee
Takematsu <kaientai100@gmail.com>
2022-05-06 17:06:13 +0900

    runserver2

--- HANAHANA_CM_ver4/settings.py
+++ HANAHANA_CM_ver4/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+#import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hana_cm.apps.HanaCmConfig',
     'bootstrap4',
-    'bootstrap_datepicker_plus',
+    #'bootstrap_datepicker_plus',
 ]
 BOOTSTRAP4 = {
     'include_jquery': True,
@@ -135,4 +135,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
